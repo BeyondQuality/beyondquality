519d6fda7ee5f0f255651088260a7f5145e66a49
Vitaly Sharovatov <vitaly.sharovatov@gmail.com>
2025-10-03 16:29:31 +0200

    now local development is a bit easier with docker

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source "https://rubygems.org"
-
-gem "jekyll-theme-cayman"
-gem "jekyll"
+gem "github-pages", group: :jekyll_plugins
+gem "webrick"
---
+++ docker-run.sh
@@ -0,0 +1,5 @@
+docker run --rm --platform=linux/amd64 \
+  -v "$PWD:/srv/jekyll" -w /srv/jekyll \
+  -p 4000:4000 -p 35729:35729 \
+  jekyll/jekyll:latest \
+  sh -lc 'bundle config set path vendor/bundle && bundle install && bundle exec jekyll serve --host 0.0.0.0 --livereload'
